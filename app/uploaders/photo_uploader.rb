bf87cab1b4025dbbe8e0c5a9c95fbb8e62093f46
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-05-15 14:26:51 +1000

    initial s3 commit

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -31,8 +31,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [1080, 1080]
 
   # Create different versions of your uploaded files:
-  version :thumb_200 do
-    process resize_to_fit: [200, 200]
+
+  version :web do
+    version :thumb    { process :resize_to_fit => [200, 200] }
+    version :full     { process :resize_to_fit => [1080,1080] }
   end
   # Create different versions of your uploaded files:
   # version :thumb do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,3 @@ User.create!([
   {email: "d@gmail.com", encrypted_password: "$2a$11$I.m886L4FULdSFxjORw0NuEbA2NxeelYf0jAhVaZerXXBrzF41Iby", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2017-05-11 04:43:07", last_sign_in_at: "2017-05-04 16:28:25", current_sign_in_ip: "::1", last_sign_in_ip: "::1"},
   {email: "e@gmail.com", encrypted_password: "$2a$11$t5goWPoGCTig9DeytsDPuuuHFd.ZP/NlYRjsDD44yVasJIMQDd9q2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2017-05-11 04:43:26", last_sign_in_at: "2017-05-04 16:30:26", current_sign_in_ip: "::1", last_sign_in_ip: "::1"}
 ])
-Hashtag.create!([
-  {styleboard_id: 1, tag: "#love"},
-  {styleboard_id: 1, tag: "#life"},
-  {styleboard_id: 1, tag: "#sick"}
-])
-ProfessionalProfile.create!([
-  {user_id: 1, message_price: 4},
-  {user_id: 2, message_price: 6},
-  {user_id: 3, message_price: 8},
-  {user_id: 4, message_price: 10},
-  {user_id: 5, message_price: 4}
-])
-Profile.create!([
-  {user_id: 1, first_name: "Anderson", last_name: ".Paak", profile_pic: nil, username: "andypaak", bio: "Smooth as a motherfucker,\r\nsuede on the inside. \r\nTell me hat the rims look like"},
-  {user_id: 2, first_name: "Mac ", last_name: "Miller", profile_pic: nil, username: "macmiller", bio: "Chillin 4 hour\r\nwatching worldstar\r\n"},
-  {user_id: 3, first_name: "A$AP", last_name: "Rocky", profile_pic: nil, username: "asaprocky", bio: "livethroughtthehustlinnnnn\r\nlife's an everyday\r\n(hustle)"},
-  {user_id: 4, first_name: "Quentin", last_name: "Tarantino", profile_pic: nil, username: "thefilmaker", bio: "The man\r\nThe myth \r\nTHe legend"},
-  {user_id: 5, first_name: "Girl", last_name: "Skater", profile_pic: nil, username: "thegirlskater", bio: "I like riding skateboards\r\nAnd bunning"}
-])
-Styleboard.create!([
-  {user_id: 1, name: "Chill", description: "#love #life #sick", public: false, photo: "tumblr_lwgi5sOHZi1qipxexo1_500.jpg"}
-])
